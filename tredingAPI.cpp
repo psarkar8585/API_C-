addf7bf005204829eaf7d0eb896e0de0bc5b951e
priti <psarkar8585@gmail.com>
2024-10-26 19:53:01 +0530

    add libraries

---
+++ tredingAPI.cpp
@@ -0,0 +1,10 @@
+#include <cpr/cpr.h> //CPR (C++ Requests) is a C++ HTTP library
+#include <nlohmann/json.hpp> //handling json in c++
+#include <websocketpp/client.hpp> //client-server library in c++
+#include <websocketpp/config/asio_no_tls_client.hpp> 
+#include <string>
+#include <iostream>
+using namespace std;
+int main(){
+
+}
